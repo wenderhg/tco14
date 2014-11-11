a6a1de3d3b7766cbf9f18ee847f4d686c79ce393
Hooberth Wender Gomes <wender@zoltur.com>
2014-11-11 13:22:33 +0000

    first commit

--- src/test/java/TaroFriendsTest.java
+++ src/test/java/TaroFriendsTest.java
@@ -18,7 +18,7 @@ public class TaroFriendsTest extends TestCase {
         assertEquals("given coordinates {-3, 0, 1} and X 3, results must be 3", 3, tester.getNumber(coordinates, X));
     }
 
-    /*
+    
     public void test_should_result_be_4(){
         int[] coordinates = {4, 7, -7};
         int X = 5;
@@ -30,13 +30,13 @@ public class TaroFriendsTest extends TestCase {
     1455	
     106, 347, -103, -482, 807, -507, -485, -582
     832
-    /
+    
     public void test_should_result_be_1204(){
         int[] coordinates = {106, 347, -103, -482, 807, -507, -485, -582};
         int X = 832;
         assertEquals("given coordinates {106, 347, -103, -482, 807, -507, -485, -582} and X 832", 1204, tester.getNumber(coordinates, X));
     }
-    
+    */
 //false	1416	1621	527, -168, 795, 113, 319, 24, 613, 661, -829, 763, 737, 541, -717, 981, -12, 512, 898, -87, 73, -968, -553, 880, 228, -586, -265, -211, -3, -252, -941, 491, -967, -766, -616, 253, -629, 293, 428, 744, -778, -203, -321, 222, 824    
 
     public void test_should_result_be_0(){
@@ -63,6 +63,6 @@ public class TaroFriendsTest extends TestCase {
         assertEquals("given coordinates {7} and X 7, results must be 0", 0, tester.getNumber(coordinates, X));
     }
     
-    */
+    
     
 }
